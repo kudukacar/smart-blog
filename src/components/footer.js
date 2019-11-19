b933daa37d1ea14cbbeab5b484e66472cb0f7799
manju <kudukacar@hotmail.com>
2019-11-19 18:03:11 -0500

    fixed icons and change background image

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,18 @@
 import React from "react";
+import { FaTwitter } from 'react-icons/fa';
+import { FaFacebook } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
+
+
 
 const Footer = () => (
   <div className="footer">
     <div>mthomas@smartmovesphysicaltherapy.com</div>
     <div>678-372-7972</div>
     <div>
-      <a href="https://www.facebook.com/floralparknewyork"><i className="fab fa-facebook-f fa-lg" /></a>
-      <a href="https://twitter.com/movesphysical"><i className="fab fa-twitter fa-lg" /></a>
-      <a href="https://www.linkedin.com/company/smart-moves-physical-therapy"><i className="fab fa-linkedin-in fa-lg" /></a>
+      <a href="https://www.facebook.com/floralparknewyork"><FaFacebook /></a>
+      <a href="https://twitter.com/movesphysical"><FaTwitter /></a>
+      <a href="https://www.linkedin.com/company/smart-moves-physical-therapy"><FaLinkedin /></a>
     </div>
     <div className="copyright">©2019 BY SMART MOVES PHYSICAL THERAPY</div>
   </div>
