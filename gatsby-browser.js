7d9aa9270a1e183731a1b9db324fbcd235dc9475
manju <kudukacar@hotmail.com>
2019-11-24 14:44:27 -0500

    make responsive navbar

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,3 +2,4 @@
 import "typeface-montserrat"
 import "typeface-merriweather"
 import "./src/styles/global.css"
+import 'bootstrap/dist/css/bootstrap.min.css'
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,25 +1,118 @@
 import React from "react";
 import logo from '../../content/assets/logo.png';
 import { Link } from 'gatsby';
+import PropTypes from "prop-types";
+import {
+  Collapse,
+  Container,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Nav
+} from "reactstrap";
+ 
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapsed: true
+    };
+  }
 
-const NavBar = () => {
-  return (
-    <header className="nav">
-      <nav className="leftnav">
-        <img src={logo} alt="in home physical therapy" />
-        <div>
-          <Link to="/">Smart Moves Physical Therapy</Link>
-        </div>
-      </nav>
-      <nav className="rightnav">
-        <Link activeClassName="selected" to="/">About</Link>
-        <Link activeClassName="selected" to="/services">Services</Link>
-        <Link activeClassName="selected" to="/insurance">Insurance</Link>
-        <Link activeClassName="selected" to="/blog">Blog</Link>
-        <Link activeClassName="selected" to="/contact">Contact</Link>
-      </nav>
-    </header>
-  )
+  toggleNavbar = () => {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
+
+  closeNavbar = () => {
+    if (this.state.collapsed !== true) {
+      this.toggleNavbar();
+    }
+  }
+
+  render() {
+    return(
+      <Container>
+        <Navbar className="navigation__navbar" light expand="md">
+            <NavbarBrand style={{ color: "#007acc", fontWeight: "bold" }} href="/">
+            <img src={logo} alt="in home physical therapy" style={{ maxWidth: 40, maxHeight: 50, marginRight: "2%" }}/>
+            Smart Moves Physical Therapy
+            </NavbarBrand>
+          <Container>
+            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+            <Collapse isOpen={!this.state.collapsed} navbar>
+              <Nav className="mr-auto" navbar>
+                  <Link
+                    style={{ color: "black" }}
+                    onClick={this.closeNavbar}
+                    className="nav-link navigation__navlinks" 
+                    to="/"
+                  >
+                    About
+                  </Link>
+                  <Link
+                    style={{ color: "black" }}
+                    onClick={this.closeNavbar}
+                    className="nav-link navigation__navlinks"
+                    to="/services"
+                  >
+                    Services
+                  </Link>
+                  <Link
+                    style={{ color: "black" }}
+                    onClick={this.closeNavbar}
+                    className="nav-link navigation__navlinks"
+                    to="/insurance"
+                  >
+                    Insurance
+                  </Link>
+                  <Link
+                    style={{ color: "black" }}
+                    onClick={this.closeNavbar}
+                    className="nav-link navigation__navlinks"
+                    to="/blog"
+                  >
+                    Blog
+                  </Link>
+                  <Link
+                  style={{ color: "black" }}
+                    onClick={this.closeNavbar}
+                    className="nav-link navigation__navlinks"
+                    to="/contact"
+                  >
+                    Contact
+                  </Link>
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </Container>
+    )
+  }
+
+}
+
+Navbar.propTypes = {
+  light: PropTypes.bool,
+  dark: PropTypes.bool,
+  fixed: PropTypes.string,
+  color: PropTypes.string,
+  role: PropTypes.string,
+  expand: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string])
+  // pass in custom element to use
+}
+
+NavbarBrand.propTypes = {
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string])
+  // pass in custom element to use
+}
+
+NavbarToggler.propTypes = {
+  type: PropTypes.string,
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string])
+  // pass in custom element to use
 }
 
 export default NavBar;
